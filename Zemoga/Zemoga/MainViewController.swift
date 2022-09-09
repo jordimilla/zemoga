fe2df62915f074aa2119cb73e4d088a6913da232
Jordi Milla <jordi.milla@bitpanda.com>
2022-09-09 16:12:08 +0200

    Add xcode project

---
+++ Zemoga/Zemoga/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zemoga
+//
+//  Created by Jordi Milla on 9/9/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
